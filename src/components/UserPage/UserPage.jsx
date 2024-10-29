82e3ca9553d5069c9475207c98e4a99fee568fbd
Munir Abshir <munirabshir@icloud.com>
2024-10-29 15:11:30 -0500

    added table set up trying out ui card so lets see how it goes

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -3,6 +3,14 @@ import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
 import axios from 'axios';
 import { useState } from 'react';
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
@@ -17,13 +25,15 @@ const [name, setname] = useState('');
 const [price, setprice] = useState('');
 const [description, setdescription] = useState('');
 const [image, setimage] = useState('');
+const [data,setData] = useState([]);
 
 
 
   function fetchData () {
   axios.get('/api/wishList')
   .then ((response) =>{
-  console.log(response.data)
+  // console.log(response.data)
+  setData(response.data)
   
   })
   .catch ((error) => {
@@ -58,6 +68,7 @@ console.log(payload);
 }
 
   return (
+    <>
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
       <p>Your ID is: {user.id}</p>
@@ -70,8 +81,41 @@ console.log(payload);
 <input type='text' name='image_url' placeholder='image url' value={image} onChange={ (e) => setimage(e.target.value)}></input>
 <button type='submit'>ADD</button>
 </form>
+
+
+<ul>
+      {data.map((item, index) => (
+        <table>
+        <tr>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Description</th>
+            <th>Image</th>
+        </tr>
+        <tr>
+            <td>{item.name}</td>
+            <td>{item.price}</td>
+            <td>{item.description}</td>
+            <td><img 
+      src={item.image_url}
+      alt="image"
+      /> {item.image_url}</td>
+        </tr>
+        
+       
+    </table>
+      ))}
+    </ul>
+
+
       <LogOutButton className="btn" />
     </div>
+
+
+
+
+
+    </>
   );
 }
 
